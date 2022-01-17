f0e28c8f1587fbbfb05aa27e4d52b7e2f28fd15f
Ubuntu <ubuntu@ip-172-31-80-151.ec2.internal>
2022-01-17 16:24:27 +0000

    modifico todolist.py

--- src/todoList.py
+++ src/todoList.py
@@ -9,14 +9,13 @@ from botocore.exceptions import ClientError
 
 def get_table(dynamodb=None):
     if not dynamodb:
-        URL = os.environ['DYNAMODB_TABLE']
+        URL = os.environ['ENDPOINT_OVERRIDE']
         if URL:
             print('URL dynamoDB:'+URL)
             boto3.client = functools.partial(boto3.client, endpoint_url=URL)
             boto3.resource = functools.partial(boto3.resource,
                                                endpoint_url=URL)
-        # dynamodb = boto3.resource("dynamodb")
-        dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+        dynamodb = boto3.resource("dynamodb")
     # fetch todo from the database
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     return table
