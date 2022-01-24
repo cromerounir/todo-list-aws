31ac42433b24338bc3d381da6ba5254cc01ec634
Ubuntu <ubuntu@ip-172-31-80-151.ec2.internal>
2022-01-24 13:06:38 +0000

    intentando subir 2401

--- src/todoList.py
+++ src/todoList.py
@@ -9,7 +9,7 @@ from botocore.exceptions import ClientError
 
 def get_table(dynamodb=None):
     if not dynamodb:
-        URL = os.environ['ENDPOINT_OVERRIDE' , 'Error']
+        URL = os.environ['ENDPOINT_OVERRIDE','Error']
         if URL:
             print('URL dynamoDB:'+URL)
             boto3.client = functools.partial(boto3.client, endpoint_url=URL)
