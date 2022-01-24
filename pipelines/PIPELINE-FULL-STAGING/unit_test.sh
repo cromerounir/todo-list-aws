845552b0c91459332097ff932e8812dd1c08d3aa
Ubuntu <ubuntu@ip-172-31-80-151.ec2.internal>
2022-01-24 12:26:50 +0000

    intentando subir coverage2401

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -4,7 +4,8 @@ source todo-list-aws/bin/activate
 set -x
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 echo "PYTHONPATH: $PYTHONPATH"
-export DYNAMODB_TABLE=todoUnitTestsTable
+#export DYNAMODB_TABLE=todoUnitTestsTable
+export ENDPOINT_OVERRIDE=http://localhost:8000
 python test/unit/TestToDo.py
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
